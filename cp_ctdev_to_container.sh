3a545348c67f82f3abec3d6fd9993ce08d5d7288
CowTwo <jashunwan01@gmail.com>
2018-03-10 19:12:42 +0800

    Initial commit

---
+++ cp_ctdev_to_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /work/gem5/ct_dev_src /usr/local/src/gem5/src
+cp -r /work/gem5/ct_dev_configs /usr/local/src/gem5/configs
+cp -r /work/gem5/ct_dev_tests /usr/local/src/gem5/tests
